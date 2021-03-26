84d9c575450886408dcc0d50be8cac4f4b6f98e9
rhday <54642787+rhday@users.noreply.github.com>
2021-03-26 11:25:47 +0000

    built out namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+    namespace :api do ## This tells the application where to look "app/controllers/api"
+      namespace :v1 do ## This tells the application where to look "app/controllers/api/v1"
+        resources :accounts do
+          resources :transactions #nesting the transaction resource within the accounts resource means that transactions can only be accessed through accounts
+        end 
+      end  
+    end 
   
-  resources :transactions
-  resources :accounts
-end
+    #namespacing routes lets the browser know where to look for the controller within the backend of the application.
+  
+  end
