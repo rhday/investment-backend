bc36e6684c73dfba43b6612b31f280dcccfda83e
rhday <54642787+rhday@users.noreply.github.com>
2021-03-26 15:14:30 +0000

    built account_controller

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::AccountsController < ApplicationController
+
+    def index
+        @accounts = Account.all
+        render json: @accounts
+    end 
+
+    def create
+        @account = Account.new(account_params)
+        if @account.save
+            render json: @account 
+        else 
+            render json: {error: 'Error creating account.'}
+        end 
+    end 
+
+    def show
+        @account = Account.find(params[:id])
+        render json: @account
+    end 
+
+    def destroy
+        @account = Account.find(params[:id])
+        @account.destroy
+    end 
+
+    private
+
+    def account_params
+        #the params require(.require) an account and permit (.permit) the attributes specified 
+        params.require(:account).permit(:name, :balance)
+    end 
+
 end
